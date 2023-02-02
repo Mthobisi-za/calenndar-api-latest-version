f8fa0e7ef571ba66314e0eea9b9ae4d7d3625cc6
Mthobisi-za <mthobisimtho333@gmail.com>
2023-02-02 14:49:48 +0200

    done

--- index.js
+++ index.js
@@ -38,8 +38,40 @@ app.get('/', (req, res) => {
     })();
 
 });
+app.get('/:name/:surname', (req, res) => {
+    var name = req.params.name;
+    var surname = req.params.surname;
+    (async() => {
+        await getDocs(collection(db, "talent")).then(resp => {
+            var list = [];
+            resp.forEach(doc => {
+                if (doc.data().talent.length == 1) {
+                    if (doc.data().talent[0].name == name && doc.data().talent[0].surname) {
+                        list.push(doc.data().talent[0]);
+                        console.log(doc.data());
+                    }
+                } else {
+                    doc.data().talent.forEach(ele => {
+                        console.log(ele);
+                        if (ele.name == name && ele.surname) {
+                            list.push(ele);
+                            console.log(ele);
+                        }
+                    });
 
-app.get('/update/:name/:surname/:email/:company_name/:e_name_surname/:occupation/:date_booked', (req, res) => {
+                }
+
+
+
+            });
+
+            res.json({ list });
+        });
+    })();
+
+});
+
+app.get('/update/:name/:surname/:email/:company_name/:e_name_surname/:occupation/:date_booked/:type', (req, res) => {
     let name = req.params.name;
     let surname = req.params.surname;
     let email = req.params.email;
@@ -47,17 +79,58 @@ app.get('/update/:name/:surname/:email/:company_name/:e_name_surname/:occupation
     let e_name_surname = req.params.e_name_surname;
     let occupation = req.params.occupation;
     let date_booked = req.params.date_booked;
-
+    let type = req.params.type;
     var fromd = (date_booked).replace('-', '/').replace('-', '/').replace('-', '/').replace('-', '/').replace('-', '/');
     var full = fromd.replace('=', '-');
-    console.log('full date >' + full);
+    let fullData = () => {
+        if (type !== 'team') {
+            return [{
+                name,
+                surname,
+                occupation,
+                email,
+                company_name,
+                client_name: e_name_surname,
+                date_booked: full
+            }]
+
+        } else {
+            var data = [];
+            var firstN = name.split('&')[0]
+            var firstSurname = surname.split('&')[0];
+            var firstRole = occupation.split('&')[0];
+            var obj = {
+                name: firstN,
+                surname: firstSurname,
+                occupation: firstRole,
+                email,
+                company_name,
+                client_name: e_name_surname,
+                date_booked: full
+            }
+            data.push(obj);
+            //second user
+            var secondN = name.split('&')[1];
+            var secondSurname = surname.split('&')[1];
+            var secondRole = occupation.split('&')[1];
+            var objj = {
+                name: secondN,
+                surname: secondSurname,
+                occupation: secondRole,
+                email,
+                company_name,
+                client_name: e_name_surname,
+                date_booked: full
+            }
+            data.push(objj);
+            return data
+        }
+    };
 
     (async() => {
         try {
             const docRef = await addDoc(collection(db, "talent"), {
-                talent: [
-                    { name, surname, email, company_name, client_name: e_name_surname, occupation, date_booked: full }
-                ]
+                talent: fullData()
             });
 
             console.log("Document written with ID: ", docRef.id);
